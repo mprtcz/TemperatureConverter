55c20e485e03e1dc5d33ae7b14b69f33320fc8a7
mprtcz <azet89@gmail.com>
2016-12-09 15:09:30 +0100

    Refactoring;

--- src/main/java/com/tem/tempConverter/Controller/Controller.java
+++ src/main/java/com/tem/tempConverter/Controller/Controller.java
@@ -1,6 +1,7 @@
 package com.tem.tempconverter.controller;
 
-import com.tem.tempconverter.converters.*;
+import com.tem.tempconverter.converters.Converter;
+import com.tem.tempconverter.converters.ConverterFactory;
 import com.tem.tempconverter.utils.ChartsDataGenerator;
 import com.tem.tempconverter.utils.Scale;
 import javafx.scene.chart.LineChart;
@@ -8,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.math.BigDecimal;
 
 /**
  * Created by Azet on 2016-06-28.
@@ -26,27 +29,35 @@ public class Controller {
         String enteredValue = enteredValueTextField.getText();
         try {
             int value = Integer.parseInt(enteredValue);
-
             Converter converter = ConverterFactory.getConverter(convertToChoiceBox.getValue());
-            double result = converter.convert(scaleChoiceBox.getValue(), value);
-
-            resultsLabel.setText(value +" degrees " +scaleChoiceBox.getValue().toString().toLowerCase()
-                    +" equals " +result +" degrees " +convertToChoiceBox.getValue().toString().toLowerCase());
-            ChartsDataGenerator chartsDataGenerator = new ChartsDataGenerator(scaleChoiceBox.getValue(), value);
-
-            lineChart.getData().clear();
-            lineChart.getData().addAll(chartsDataGenerator.getChartData(convertToChoiceBox.getValue()));
-            lineChart.getData().addAll(chartsDataGenerator.getChartInitialValue(convertToChoiceBox.getValue()));
-
-            convertFromLabel.setText(scaleChoiceBox.getValue().toString());
-            convertToLabel.setText(convertToChoiceBox.getValue().toString());
+            drawChart(value);
+            setLabel(value, converter);
 
         } catch (NumberFormatException nex) {
             enteredValueTextField.setText("Please type only numeric values");
         }
     }
 
+    private void setLabel(int value, Converter converter) {
+        BigDecimal result = new BigDecimal(converter.convert(scaleChoiceBox.getValue(), value));
+        result = result.setScale(2, BigDecimal.ROUND_HALF_UP);
 
+        resultsLabel.setText(value +" degrees " +scaleChoiceBox.getValue().toString().toLowerCase()
+                +" equals " +result +" degrees " +convertToChoiceBox.getValue().toString().toLowerCase());
+    }
+
+    private void drawChart(int value) {
+        ChartsDataGenerator chartsDataGenerator = new ChartsDataGenerator(scaleChoiceBox.getValue(), value);
+        lineChart.getData().clear();
+        lineChart.getData().addAll(chartsDataGenerator.getChartData(convertToChoiceBox.getValue()));
+        lineChart.getData().addAll(chartsDataGenerator.getChartInitialValue(convertToChoiceBox.getValue()));
+        setChartAxisNames();
+    }
+
+    private void setChartAxisNames() {
+        convertFromLabel.setText(scaleChoiceBox.getValue().toString());
+        convertToLabel.setText(convertToChoiceBox.getValue().toString());
+    }
 
     public void initialize() {
         //populate scaleChoiceBox
